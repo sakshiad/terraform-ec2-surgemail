72ec7fc93f5dd1a89a2a56ca2ef65c0d780cd2b6
sakshiad <sakshi@cloudtechservice.com>
2024-06-28 14:09:40 +0545

    Add gitignore

---
+++ ec2.tf
@@ -0,0 +1,20 @@
+module "ec2_instance" {
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-ec2-instance.git"
+
+  name = "mail-instance"
+  ami  = "ami-09040d770ffe2224f"
+
+  instance_type = "t2.micro"
+  key_name      = "sakshi-mail"
+
+  monitoring             = false
+  vpc_security_group_ids = ["sg-033f4f527ebc6646b"]
+  subnet_id              = "subnet-03785cda9938c768d"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+    Owner       = "DevOps-sakshi"
+  }
+}
+
